bdc7b81e2aee3b20577bb4853a0b3f8ba1691a15
Athena2006 <athenadaguman45@gmail.com>
2026-09-20 16:34:41 +0800

    added a header input data

---
+++ react-demo/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Daily Expense Tracker</h1>
+      <p>Track your food, transportation, and other expenses.</p>
+    </header>
+  );
+}
+
+export default Header;
